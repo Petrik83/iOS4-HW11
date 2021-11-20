a27a063d68f7c2e3d2d4ba22f1a6c7f5e1978a8a
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2021-11-20 22:37:19 +0300

    Progress bar start working.

--- iOS4-HW11/ViewController.swift
+++ iOS4-HW11/ViewController.swift
@@ -12,8 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @objc func animateProgress() {
+           let cp = self.view.viewWithTag(101) as! CircularProgress
+           cp.setProgressWithAnimation(duration: 1.0, value: 0.8)
+       }
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        view.backgroundColor = UIColor.gray
+        let circularProgress = CircularProgress(frame: CGRect(x: 10.0, y: 30.0, width: 100.0, height: 100.0))
+                circularProgress.progressColor = UIColor(red: 52.0/255.0, green: 141.0/255.0, blue: 252.0/255.0, alpha: 1.0)
+                circularProgress.trackColor = UIColor(red: 52.0/255.0, green: 141.0/255.0, blue: 252.0/255.0, alpha: 0.6)
+                circularProgress.tag = 101
+                circularProgress.center = self.view.center
+                self.view.addSubview(circularProgress)
+                
+                //animate progress
+                self.perform(#selector(animateProgress), with: nil, afterDelay: 1)
+        
+    }
+    
+    
 }
 
